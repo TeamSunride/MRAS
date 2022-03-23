926106a4bf80e8774cbb3e6491ae418cbabc0c2f
Alexander Becker <nabla.becker@mailbox.org>
2022-03-23 21:46:10 +0000

    finished the implementation of the most basic 3 stage pipeline

--- lib/example.cc
+++ lib/example.cc
@@ -5,10 +5,16 @@
 #include "node.h"
 #include "Threshold_Filter.h"
 
+#define ITERATIONS 10000
+
 int main() {
 	RandomNumberSensor rns(MeasurementType::Pressure);
 	Threshold_Filter thresh_filter(3.0, MeasurementType::Pressure);
 	FileSink output("test_out.txt");
 	rns.pass_result_to(&thresh_filter);
+	thresh_filter.pass_result_to(&output);
+	for (int i=0; i < ITERATIONS; i++) {
+		rns.get_measurement();
+	}
 	return 0;
 }

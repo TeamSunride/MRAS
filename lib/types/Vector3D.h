86593e6be14bd76708ba45db1c02bda9821519fa
Tom Danvers <tbodanvers1@sheffield.ac.uk>
2022-04-12 20:31:51 +0100

    fix Vector3D

--- lib/types/Vector3D.h
+++ lib/types/Vector3D.h
@@ -22,38 +22,4 @@ public:
 };
 
 
-Vector3D Vector3D::divideScalar(float factor) {
-    Vector3D returnVector{};
-    for (int i=0; i<3; i++) {
-        returnVector._dimensions[i] = _dimensions[i] / factor;
-    }
-    return returnVector;
-}
-
-template<typename T>
-float Vector3D::getX() {
-    return _dimensions[0];
-}
-
-float Vector3D::getY() {
-    return _dimensions[1];
-}
-
-float Vector3D::getZ() {
-    return _dimensions[2];
-}
-
-void Vector3D::setX(float value) {
-    _dimensions[0] = value;
-}
-
-void Vector3D::setY(float value) {
-    _dimensions[1] = value;
-}
-
-void Vector3D::setZ(float value) {
-    _dimensions[2] = value;
-}
-
-
 #endif //MRASV2_VECTOR3D_H

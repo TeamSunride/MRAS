855f56011a5ab01eb464bc66285de5bcb691ad44
TomD53 <tomtomgaming101@gmail.com>
2022-04-19 18:49:20 +0100

    add another unit test for RTC range to check that the timestamp was after Tue Apr 19 2022 18:35:36 GMT+0000

--- test/test_teensy_rtc/test_teensy_rtc.cpp
+++ test/test_teensy_rtc/test_teensy_rtc.cpp
@@ -11,14 +11,25 @@ void test_timestamp() {
     Serial.println("t2: " + int64String(t2));
 
     // we expect dT to equal 1000ms
-    TEST_ASSERT_EQUAL(t2-t1, 1000);
+    TEST_ASSERT_EQUAL(t2 - t1, 1000);
 }
 
-void setup()
-{
-	UNITY_BEGIN();
+void test_range() {
+    uint64_t t1 = getTimestampMillis();
+    uint64_t THRESHOLD = 1650393336773ULL; // Tue Apr 19 2022 18:35:36 GMT+0000
+
+    // we expect the result to be a time after Tue Apr 19 2022 18:35:36 GMT+0000
+    TEST_ASSERT_TRUE_MESSAGE(t1 > THRESHOLD, "The RTC did not sync properly. Make sure you are using "
+                                             "teensy-gui and not teensy-cli as the upload_protocol. "
+                                             "The time reported by the RTC was before Tue Apr 19 2022 18:35:36 GMT+0000");
+}
+
+
+void setup() {
+    UNITY_BEGIN();
 
     RUN_TEST(test_timestamp);
+    RUN_TEST(test_range);
 
     UNITY_END();
 }

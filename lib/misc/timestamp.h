0e1b09445921bde3451217c84b2ade3f099d825a
Tom Danvers <tomtomgaming101@gmail.com>
2022-05-07 23:09:55 +0100

    move function definitions to timestamp.cpp

--- lib/misc/timestamp.h
+++ lib/misc/timestamp.h
@@ -2,47 +2,23 @@
 // 2022 TeamSunride.
 //
 
+
+#ifndef TIMESTAMP_H
+#define TIMESTAMP_H
+
 #include <Arduino.h>
 #include <time.h>
 #include <TimeLib.h>
 
-#ifndef TIMESTAMP_H
-#define TIMESTAMP_H
 
 
 /**
  * @return UNIX time in milliseconds
  */
-uint64_t getTimestampMillis()
-{
-	uint64_t periods;
-	uint32_t hi1 = SNVS_HPRTCMR, lo1 = SNVS_HPRTCLR;
-	while (true)
-	{
-		uint32_t hi2 = SNVS_HPRTCMR, lo2 = SNVS_HPRTCLR;
-		if (lo1 == lo2 && hi1 == hi2)
-		{
-			periods = (uint64_t)hi2 << 32 | lo2;
-			break;
-		}
-		hi1 = hi2;
-		lo1 = lo2;
-	}
-	uint32_t ms = (1000 * (periods % 32768)) / 32768;
-	time_t sec = periods / 32768;
-	tm t = *gmtime(&sec);
-
-	setTime(t.tm_hour, t.tm_min, t.tm_sec, t.tm_mday, t.tm_mon + 1, t.tm_year + 1900);
-
-	uint64_t unixTime = (uint64_t(now()) * 1000) + ms;
-	return unixTime;
-}
+uint64_t getTimestampMillis();
 
 
-time_t getTeensy3Time()
-{
-    return Teensy3Clock.get();
-}
+time_t getTeensy3Time();
 
 
 #endif //TIMESTAMP_H

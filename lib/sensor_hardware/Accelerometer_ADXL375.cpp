3bdfb651ed5b9112fdfd81ca6fc81ba6eca74573
TomD53 <tbodanvers@gmail.com>
2022-11-17 21:55:45 +0000

    change to uint

--- lib/sensor_hardware/Accelerometer_ADXL375.cpp
+++ lib/sensor_hardware/Accelerometer_ADXL375.cpp
@@ -41,9 +41,9 @@ int8_t Accelerometer_ADXL375::readData() {
 
 
     Vector<int16_t, 3> rawAccel = {
-            (int16_t) ((int16_t)buffer[1] << 8 | ((int16_t)buffer[0])),
-            (int16_t) ((int16_t)buffer[3] << 8 | (int16_t)buffer[2]),
-            (int16_t) ((int16_t)buffer[5] << 8 | (int16_t)buffer[4])
+            (int16_t) ((uint16_t)buffer[1] << 8 | ((uint16_t)buffer[0])),
+            (int16_t) ((uint16_t)buffer[3] << 8 | (uint16_t)buffer[2]),
+            (int16_t) ((uint16_t)buffer[5] << 8 | (uint16_t)buffer[4])
     };
 
 

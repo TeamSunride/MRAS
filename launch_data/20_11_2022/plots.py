081af1bd45ad06cd082a574a08856c52c358a267
TomD53 <tbodanvers@gmail.com>
2022-11-22 17:17:57 +0000

    add launch data

---
+++ launch_data/20_11_2022/plots.py
@@ -0,0 +1,35 @@
+import pandas
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+plt.style.use("ggplot")
+
+df = pandas.read_csv("output.csv", usecols=range(1, 20))
+df["time_seconds"] = df["timestamp"].subtract(df["timestamp"][0]).div(1000)
+print(df)
+
+
+df.plot("time_seconds", y=["imuAX", "imuAY", "imuAZ"], xlabel="Time (s)", ylabel="Acceleration (m/s)",
+        title="Acceleration")
+plt.show()
+
+df.plot("time_seconds", y=["imuGX", "imuGY", "imuGZ"], xlabel="Time (s)", ylabel="Gyroscope (unit?)",
+        title="Gyroscope Data")
+plt.show()
+
+df.plot("time_seconds", y=["imuMX", "imuMY", "imuMZ"], xlabel="Time (s)", ylabel="Magnetometer value",
+        title="Magnetometer Data")
+plt.show()
+
+df.plot("time_seconds", y=["pressure"], xlabel="Time (s)", ylabel="Pressure (Pa)",
+        title="Barometer data")
+plt.show()
+
+df.plot("time_seconds", y=["h"], xlabel="Time (s)", ylabel="Altitude (m)",
+        title="Barometric altitude")
+plt.show()
+
+df["h_adjusted"] = df["h"] - df["h"][0]
+df.plot("time_seconds", y=["h_adjusted"], xlabel="Time (s)", ylabel="Altitude (m)",
+        title="Barometric altitude (adjusted)")
+plt.show()

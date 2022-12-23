a0becaf0e874f9a4873b8dab719f302849566fbd
robos <robosam2003@gmail.com>
2022-12-23 12:18:40 +0000

    fix latitude, longitude and altitude conversion (10e-8 not 10e-7,  10e-4 not 10e-3)

--- lib/Sensor_ZOEM8Q/Sensor_ZOEM8Q.cpp
+++ lib/Sensor_ZOEM8Q/Sensor_ZOEM8Q.cpp
@@ -12,7 +12,7 @@ int8_t Sensor_ZOEM8Q::setup() {
         log("GNSS not detected at default I2C address. Please check wiring.");
         return (int8_t) 1; // failure;
     }
-    gnss->setNavigationFrequency(1); // 5 Hz update rate
+    gnss->setNavigationFrequency(5);
     gnss->setAutoPVT(true); // Tell the GNSS to "send" each solution
     return 0; // success
 }
@@ -21,11 +21,11 @@ int8_t Sensor_ZOEM8Q::loop() {
     if (gnss->getPVT() &&  !gnss->getInvalidLlh() ) {
 
         auto *gnss_msg = new GNSSDataMsg(GNSSDataMsg_t);
-        gnss_msg->latitude = static_cast<float> (gnss->getLatitude() * (10e-7)); // degrees
-        gnss_msg->longitude = static_cast<float> (gnss->getLongitude() * (10e-7)); // degrees
+        gnss_msg->latitude = (float) (gnss->getLatitude() * (10e-8)); // degrees
+        gnss_msg->longitude = (float) (gnss->getLongitude() * (10e-8)); // degrees
 
         // this is the height above the WGS84 ellipsoid, not mean sea level - which is best?
-        gnss_msg->altitude = static_cast<float> (gnss->getAltitude() * (10e-3)); // metres
+        gnss_msg->altitude = (float) (gnss->getAltitudeMSL() * (10e-4)); // metres
 
         gnss_msg->fixType = gnss->getFixType();
         gnss_msg->SIV = gnss->getSIV();

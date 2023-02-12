fa02a194d6d5c089153eded452f98376df4940fb
nikiexpo <85587378+nikiexpo@users.noreply.github.com>
2023-02-12 00:48:39 +0000

    Deploying to gh-pages from @ TeamSunride/MRAS@db9f449ed1903e09571b6b153f999aa3f5c6996f 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -3,5 +3,6 @@ var searchData=
   ['mag_0',['mag',['../structMagnetometerDataMsg.html#a255e523f6b17423fbd847b4d16dfaab7',1,'MagnetometerDataMsg']]],
   ['magnetometerdatamsg_1',['MagnetometerDataMsg',['../structMagnetometerDataMsg.html',1,'']]],
   ['mras_20_28multi_20rocket_20avionics_20system_29_20documentation_2',['MRAS (Multi Rocket Avionics System) Documentation',['../index.html',1,'']]],
-  ['mras_5fsystem_3',['MRAS_System',['../classMRAS__System.html',1,'MRAS_System'],['../classMRAS__System.html#a539e7079c645c20af80ab37b212cd561',1,'MRAS_System::MRAS_System()']]]
+  ['mras_20code_20to_20a_20beginner_3',['MRAS code to a Beginner',['../md_docs_BeginnerExplanation.html',1,'']]],
+  ['mras_5fsystem_4',['MRAS_System',['../classMRAS__System.html',1,'MRAS_System'],['../classMRAS__System.html#a539e7079c645c20af80ab37b212cd561',1,'MRAS_System::MRAS_System()']]]
 ];
--- search/pages_3.js
+++ search/pages_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['mras_20_28multi_20rocket_20avionics_20system_29_20documentation_0',['MRAS (Multi Rocket Avionics System) Documentation',['../index.html',1,'']]]
+  ['mras_20_28multi_20rocket_20avionics_20system_29_20documentation_0',['MRAS (Multi Rocket Avionics System) Documentation',['../index.html',1,'']]],
+  ['mras_20code_20to_20a_20beginner_1',['MRAS code to a Beginner',['../md_docs_BeginnerExplanation.html',1,'']]]
 ];

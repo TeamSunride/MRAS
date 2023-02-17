f39a78e9d1b38619c1559ed1cfb42bddaf765127
Tom Danvers <32431576+TomD53@users.noreply.github.com>
2023-02-17 22:19:36 +0000

    make the buffer names more explicit

--- test/native/test_cobs_encoding/test_cobs_encoding.cpp
+++ test/native/test_cobs_encoding/test_cobs_encoding.cpp
@@ -18,23 +18,24 @@ void print_buffer(uint8_t *buffer, size_t length) {
 }
 
 void test_cobs_encoding() {
-    uint8_t buffer[255] = {};
+    uint8_t encoded_buffer[255] = {};
+    uint8_t decoded_buffer[255] = {};
     uint8_t input_data[] = {1, 0, 3, 4, 5, 0, 1};
 
     printf("Input data: \t");
     print_buffer(input_data, sizeof input_data);
 
-    uint8_t encoded_length = cobsEncode(input_data, sizeof input_data, buffer);
+    uint8_t encoded_length = cobsEncode(input_data, sizeof input_data, encoded_buffer);
 
     printf("Encoded data: \t");
-    print_buffer(buffer, encoded_length);
+    print_buffer(encoded_buffer, encoded_length);
 
-    uint8_t decoded_length = cobsDecode(buffer, encoded_length, buffer);
+    uint8_t decoded_length = cobsDecode(encoded_buffer, encoded_length, decoded_buffer);
 
     printf("Decoded data: \t");
-    print_buffer(buffer, decoded_length);
+    print_buffer(decoded_buffer, decoded_length);
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(input_data, buffer, sizeof input_data);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(input_data, decoded_buffer, sizeof input_data);
 }
 
 int main() {
